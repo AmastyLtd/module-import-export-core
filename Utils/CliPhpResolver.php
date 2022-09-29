ce0c3a3c2ca3ad22e4a043e2247d693214992d0f
Eduard Muradov <eduard.muradov@amasty.com>
2022-09-29 21:33:35 +0300

    Release 1.1.4

--- Utils/CliPhpResolver.php
+++ Utils/CliPhpResolver.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Amasty\ImportExportCore\Utils;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\DeploymentConfig;
 use Magento\Framework\Shell;
 use Symfony\Component\Process\PhpExecutableFinder;
 
 class CliPhpResolver
 {
-    private const PHP_EXECUTABLE_PATH = 'php_executable_path';
+    private const ENV_PHP_EXECUTABLE_PATH = 'php_executable_path';
+    private const CONFIG_PHP_EXECUTABLE_PATH = 'amasty_base/system/cli_php_path';
 
     private const VERSION_CHECK_REGEXP = '/PHP [\d\.\+a-z-]+ \(cli\)/';
 
@@ -30,18 +32,25 @@ class CliPhpResolver
     private $shell;
 
     /**
+     * @var ScopeConfigInterface
+     */
+    private $configProvider;
+
+    /**
      * @var string
      */
     private $executablePath;
 
     public function __construct(
         DeploymentConfig $deploymentConfig,
+        ScopeConfigInterface $configProvider,
         PhpExecutableFinder $executableFinder,
         Shell $shell
     ) {
         $this->deploymentConfig = $deploymentConfig;
         $this->executableFinder = $executableFinder;
         $this->shell = $shell;
+        $this->configProvider = $configProvider;
     }
 
     /**
@@ -62,7 +71,8 @@ class CliPhpResolver
     private function resolvePhpExecutable()
     {
         $pathCandidates = [
-            $this->deploymentConfig->get(self::PHP_EXECUTABLE_PATH),
+            $this->configProvider->getValue(self::CONFIG_PHP_EXECUTABLE_PATH),
+            $this->deploymentConfig->get(self::ENV_PHP_EXECUTABLE_PATH),
             $this->executableFinder->find()
         ];
 
---
+++ Utils/OptionsProcessor.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Amasty\ImportExportCore\Utils;
+
+class OptionsProcessor
+{
+    public function process(array $options, bool $allowValueIsArray = true): array
+    {
+        $preparedOptions = [];
+
+        foreach ($options as $option) {
+            if (!is_array($option)) {
+                continue;
+            }
+
+            if ($this->isValid($option, $allowValueIsArray)) {
+                $option['value'] = !is_array($option['value'])
+                    ? (string)$option['value']
+                    : $option['value'];
+
+                $preparedOptions[] = $option;
+            } else {
+                array_push($preparedOptions, ...$this->process($option));
+            }
+        }
+
+        return $preparedOptions;
+    }
+
+    private function isValid(array $option, bool $allowValueIsArray): bool
+    {
+        return isset($option['value'])
+            && ($allowValueIsArray || !is_array($option['value']));
+    }
+}
